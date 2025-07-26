ff6058616a2c419fd1794bb292ff5d45970b8fef
bryanerayner <bryanerayner@gmail.com>
2025-07-26 12:35:52 -0500

    Small change to generate cache

--- checker/examples/generate_cache.rs
+++ checker/examples/generate_cache.rs
@@ -9,13 +9,26 @@ use std::{
 use ezno_checker::{generate_cache, synthesis::EznoParser};
 
 fn main() {
-	let input = env::args().nth(1).expect("expected path to definition file");
-	let output = env::args().nth(2).expect("expected output path");
+	
+	let input = env::args().nth(1).or_else(|| {Some("./checker/definitions/overrides.d.ts".to_string())}).unwrap();
+	let output = env::args().nth(2).or_else(|| {Some("./checker/definitions/internal.ts.d.bin".to_string())}).unwrap();
 
 	// This reader, doesn't lookup in the cache
 	let reader = |path: &std::path::Path| read_to_string(path).ok();
 
-	let cache = generate_cache::<_, EznoParser>(PathBuf::from(input).as_path(), &reader, ());
-	write(output, cache).unwrap();
-	eprintln!("Cache generated 🏧💵✅");
+	let cache = generate_cache::<_, EznoParser>(
+		PathBuf::from(input).as_path(), 
+		&reader, 
+		()
+	);
+	let result = write(output, cache);
+	
+	match result {
+		Ok(()) => {
+			eprintln!("Cache generated 🏧💵✅");
+		},
+		Err(e) => {
+			eprintln!("Failed to write cache: {}", e);
+		}
+	}
 }
